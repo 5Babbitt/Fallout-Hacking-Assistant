dbac332f0ec1e04d268291fd7f5a3c19f16cc52b
Owen Harbert <owen.harbert2@gmail.com>
2024-06-08 00:19:34 +0200

    add options list entry

--- fallout_hacking_assistant.py
+++ fallout_hacking_assistant.py
@@ -8,21 +8,32 @@
 # crossout words with no similarities
 # recommend best choices
 
-options: list[str]
-password_length: int
+
+options: list[str] = []
+password_length: int = 0
 
 def main() -> None:
+    # Get each option and word length
     while True:
         option: str = input("Enter option (leave blank to finish):")
-        if len(option) <= 0 or option is None:
+        if not option:
             break
 
-        if len(password_length) <= 0 or password_length is None:
+        if len(password_length) <= 2:
+            con: str = input(f"Confirm {password_length} is the correct length (y/n)?:")
+            
+            if con.lower() != 'y':
+                print("Please Retry!")
+                continue
+
             password_length = len(option)
         elif len(option) != password_length:
             print("Incorrect word length")
             continue
 
+        options.append(option)
+
+    # print list
     ...
 
 if __name__ == "__main__":
